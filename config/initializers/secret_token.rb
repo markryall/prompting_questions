6b7a0fbb9531c62ba896bd405b85587b3175876e
Mark Ryall <mark@ryall.name>
2013-07-01 20:13:27 +1000

    empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PromptingQuestions::Application.config.secret_key_base = 'c683d678a81f6775b6b9abc777288b82f7dba0916687dc750b55a3592784b0d8c18d1d09d979df27feb9a4a0ab987370fce7a397cf1e8e302480e49a591d1283'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PromptingQuestions::Application.config.session_store :cookie_store, key: '_prompting_questions_session'
