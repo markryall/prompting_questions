d40ed67340643b177de92c415a963e69bba6a778
Mark Ryall <mark@ryall.name>
2013-07-01 23:29:14 +1000

    Added a simple auth mechanism

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,34 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
+    @questions = [] unless valid_token?
   end
 
   def show
     @question = Question.find params[:id]
+    redirect_to '/' unless valid_token?
   end
 
   def answer
+    unless valid_token?
+      redirect_to '/'
+      flash[:error] = "Sorry!"
+      return
+    end
     Answer.new.tap do |answer|
       answer.content = params[:content]
       answer.question_id = params[:id]
       answer.save
-      flash[:success] = "Thanks for answering this question!"
+      flash[:success] = "Thanks for sharing your story!"
     end
-    redirect_to '/'
+    redirect_to "/?token=#{token}"
+  end
+private
+  def token
+    @token = params[:token]
+  end
+
+  def valid_token?
+    token == ENV["TOKEN"]
   end
 end
